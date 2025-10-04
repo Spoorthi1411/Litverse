ad2039eb09dc1f983d3ef0802d9c86b32305bfc4
Spoorthi1411 <spoorthikoutam14@gmail.com>
2025-10-04 22:41:05 +0530

    Place Order Page

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import {products} from "../assets/assets.js";
 import {createContext,useState} from "react";
+import { useNavigate } from 'react-router-dom';
 
 export const ShopContext = createContext();
 
@@ -8,6 +9,7 @@ const ShopContextProvider = (props) => {
 
     const currency = '$';
     const [cartItems,setCartItems]= useState({});
+    const navigate = useNavigate();
 
     const addToCart = async(itemId)=>{
         let cartData = structuredClone(cartItems);
@@ -32,12 +34,30 @@ const ShopContextProvider = (props) => {
         setCartItems(cartData);
     }
 
+    const getCartAmount = ()=>{
+        let totalAmount = 0;
+        for(const items in cartItems){
+            let itemInfo = products.find((product)=>product._id === items);
+            for(const item in cartItems[items]){
+                try {
+                    if(cartItems[items][item]>0){
+                        totalAmount += itemInfo.price * cartItems[items][item];
+                    }
+                } catch (error) {
+                    
+                }
+            }
+        }
+        return totalAmount;
+    }
+
 
     const value = {
         products,
         currency,
         cartItems,addToCart,
         updateQuantity,
+        getCartAmount,navigate,
     }
   return (
     <ShopContext.Provider value={value}>
