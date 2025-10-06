0bbfd917dc9f0fc951339036a2e653e4c03271dc
ipsika Rai <lipsikarai1112@gmail.com>
2025-10-06 17:52:31 +0530

    heropage

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -3,7 +3,9 @@ import menu_icon from './menu_icon.png';
 import logo from './logo.jpg';
 import dropdown_icon from './dropdown_icon.png';
 import search_icon from './search_icon.png';
-import profile_icon from './profile_icon.png'
+import profile_icon from './profile_icon.png';
+import Hero_img from './Hero_img.png';
+import img_1 from './img_1.png';
 
 export const assets = {
     cart_icon,
@@ -11,7 +13,10 @@ export const assets = {
     logo,
     profile_icon,
     menu_icon,
-    search_icon
+    search_icon,
+    Hero_img,
+    img_1,
+
 }
 
 export const products = [
@@ -25,4 +30,115 @@ export const products = [
         date: 1716634345448,
         bestseller: true
     },
+    {
+        _id: "aaaab",
+        name: "The Vanishing Cherry Blossom",
+        description: "Uplifting, poignant healing fiction",
+        price: 280,
+        image: [img_1],
+        category: "Motivating",
+        date: 1716634345441,
+        bestseller: true
+    },
+    {
+       
+        _id: "aaaac",
+        name:"The Second Chance Convenience Store",
+        description: " The Second Chance Convenience Store:Dok-go's realistic and encouraging relationship with the store's customers, who have their problems and aspirations resolved so well through the pages, wins everyone's hearts ― The Korea Times A moving story about solidarity and the healing process -- Yes24 and Kyobo Book Center ",
+        price: 310,
+        image: [image_2],
+        category: "Contemporary / Inspirational / Literary",
+        date: 1035032899,
+        bestseller: true
+
+    },
+     {
+       
+        _id: "aaaad",
+        name:" you got this",
+        description: " you got this :in order to love uncondionally, we must learn first to love and forgive ourselves. This book shows us how- Farhan Akhtar",
+        price: 275,
+        image: [img_3],
+        category: "Art of Loving Yourself | Emotional wellness | Mental health | Happiness | Self-Care | Personal growth | Mindfulness | Motivational | Positive...",
+        date: 1036732899,
+        bestseller: true
+
+    },
+     {
+       
+        _id: "aaaae",
+        name:" The Chibineko Kitchen",
+        description: "The soul-nourishing Japanese hit novel about the healing power of food. ",
+        price: 400,
+        image: [img_3],
+        category: "self awarness",
+        date: 1032232899,
+        bestseller: true
+
+    },
+{
+        _id: "aauya",
+        name: "ikigai",
+        description: "Japanese Secret Art of Staying Young & Healthy (A Practical Guide to Long and Happy Life ",
+        price: 199,
+        image: [img_5],
+        category: "Romance",
+        date: 2716634345418,
+        bestseller: true
+    },
+     {
+        _id: "aaaed",
+        name: "Before the Coffee Gets Cold",
+        description: " The book takes place in a small café in Tokyo where customers can travel back in time, but only while their coffee stays warm.Each chapter follows a different person (a lover, a wife, a sister, a mother) who wants to revisit a moment from the past — not just for love, but for closure, regret, or forgiveness.",
+        price: 189,
+        image: [img_6],
+        category: "Magical Realism / Emotional Drama",
+        date: 1716634345448,
+        bestseller: true
+    },
+{
+        _id: "aaaeu",
+        name: "Night Train to the Star",
+        description: " **Story:** *Night Train to the Stars* follows a lonely boy named **Giovanni** who boards a mysterious train traveling through the Milky Way with his friend **Campanella**, symbolizing a dreamlike journey about **life, friendship, death, and spiritual awakening**.",
+        price: 275,
+        image: [img_7],
+        category: "Fantasy Fiction.",
+        date: 4716634345448,
+        bestseller: true
+    },
+     
+{
+        _id: "aaaet",
+        name: "BANARAS An Eternal Love Story",
+        description: "deep love ",
+        price: 288,
+        image: [img_8],
+        category: "Love",
+        date: 3716634345440,
+        bestseller: true
+    },
+{
+        _id: "caaaa",
+        name: "You Only Live Once",
+        description: "You Only Live Once by Stuti Changle is a contemporary Indian novel set in Goa, following three characters—Alara, Aarav, and Ricky—on a journey of love, self-discovery, and uncovering hidden pasts. It explores friendship, passion, and personal growth with emotional depth and relatable storytelling.",
+        price: 155,
+        image: [img_10],
+        category: "Health, Family & Personal Development,Self-Help,Self-Esteem",
+        date: 7716634345448,
+        bestseller: true
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ]
